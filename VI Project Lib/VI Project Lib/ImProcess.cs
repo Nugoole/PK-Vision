5fed412c8e35baf5907c56c8d42b31ce72d82712
허준도 <jundo123@live.co.kr>
2019-07-21 13:38:41 +0900

    main struct and barcode detection

---
+++ VI Project Lib/VI Project Lib/ImProcess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using ZXing;
+
+namespace VI_Project_Lib
+{
+    public class ImProcess
+    { 
+        public static Mat readImage(string filepath, ImreadModes mode)
+        {
+            return CvInvoke.Imread(filepath, mode);
+        }
+
+        public static void printImage(string windowName, Mat image)
+        {
+            CvInvoke.Imshow(windowName, image);
+        }
+        
+        public static string getBarcodeCode(string filepath)
+        {
+            Bitmap bitmap = new Bitmap(filepath);
+            IBarcodeReader reader = new BarcodeReader();
+
+            Result result = reader.Decode(bitmap);
+
+            if (result != null)
+                return result.Text;
+            else
+                return "No Barcodes";
+        }
+    }
+}
---
+++ VI Project Lib/Visual Inspection/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VI_Project_Lib;
+
+namespace Visual_Inspection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string filepath = "d:\\opencv\\pcb2.jpg";
+            ImProcess.printImage("img", ImProcess.readImage(filepath,Emgu.CV.CvEnum.ImreadModes.AnyColor));
+            MessageBox.Show(ImProcess.getBarcodeCode(filepath));
+
+        }
+    }
+}
